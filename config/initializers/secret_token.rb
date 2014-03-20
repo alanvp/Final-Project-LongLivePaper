b07b72d7fb8e7989a923080527bee93383396818
alanvp <apvanpelt@yahoo.com>
2014-03-20 16:09:14 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LongLivePaper::Application.config.secret_key_base = 'a903ecbbf2c8b882aa29a7d7af0e3a7e43f92814c794dcb58c3906c443da799628931d2783e9de3dd35b48928fe9f4ae1558bf061ef82a31712514fb0246ca88'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LongLivePaper::Application.config.session_store :cookie_store, key: '_longLivePaper_session'
